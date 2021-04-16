c8c7c228e3e48f66cfbf7d1a013343bc200fe323
Aks01singh <66474067+Aks01singh@users.noreply.github.com>
2021-04-16 22:08:12 +0530

    first commit from older repo to new

---
+++ AR Classroom Class 9/Assets/9 Scripts/ChangeScene_9.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene_9 : MonoBehaviour
+{
+    public void ChangeScene1() {
+        SceneManager.LoadScene("hetero", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene("autotrophic", LoadSceneMode.Single);
+    }
+
+    public void ChangeHomeScene()
+    {
+        SceneManager.LoadScene("homeScene", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene3()
+    {
+        SceneManager.LoadScene("nutrition in amoeba", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene4()
+    {
+        SceneManager.LoadScene("nutrition in human", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene5()
+    {
+        SceneManager.LoadScene("Respiration in plants", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene6()
+    {
+        SceneManager.LoadScene("Respiration", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene7()
+    {
+        SceneManager.LoadScene("Soil-Profile", LoadSceneMode.Single);
+    }
+    public void ChangeScene8()
+    {
+        SceneManager.LoadScene("Sun dial", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene9()
+    {
+        SceneManager.LoadScene("teeth", LoadSceneMode.Single);
+    }
+
+   
+    public void ChangeScene10()
+    {
+        SceneManager.LoadScene("Transyst plants", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene11()
+    {
+        SceneManager.LoadScene("ruminant", LoadSceneMode.Single);
+    }
+
+
+    public void ChangeScene12()
+    {
+        SceneManager.LoadScene("HeatFlow", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene13()
+    {
+        SceneManager.LoadScene("AnimalFiber2", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene14()
+    {
+        SceneManager.LoadScene("AnimalFiber1", LoadSceneMode.Single);
+    }
+
+    public void ChangeScene15()
+    {
+        SceneManager.LoadScene("moth", LoadSceneMode.Single);
+    }
+
+
+    public void ChangeScene16()
+    {
+        SceneManager.LoadScene("Caterpiller", LoadSceneMode.Single);
+    }
+
+    public void MyAppQuite()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ AR Classroom Class 9/Assets/9 Scripts/rotate_9.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_9 : MonoBehaviour
+{
+    public float speed_x = 0f;
+    public float speed_y = 0f;
+    public float speed_z = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * speed_x, 0, 0);
+        transform.Rotate(0, Time.deltaTime * speed_y, 0);
+        transform.Rotate(0, 0, Time.deltaTime * speed_z);
+    }
+}
